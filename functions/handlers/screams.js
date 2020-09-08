367695e539c1d61f87048788979fc2ad284a82f9
KyeongHoSeong <dasomaru@gmail.com>
2020-09-09 03:48:50 +0900

    lessong-07-02

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -1,49 +1,55 @@
-const { db } = require('../utils/admin')
+const {db} = require ('../utils/admin')
 
-exports.getAllscreams = (request, response) => {
-    db
-      .collection('screams')
-      .orderBy('createdAt', 'desc')
-      .get()
-      .then(data => {
-        let screams = [];
-        data.forEach(doc => {
-          //screams.push(doc.data());
-          screams.push({
-            screamId: doc.id,
-            body: doc.data().body,
-            userHandle: doc.data().userHandle,
-            createdAt: doc.data().createdAt
-          });
+exports.getAllScreams = (req, res) => {
+  db.collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        //screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
         });
-        return response.json(screams);
-      })
-      .catch(err => {
-        console.error(err);
       });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+};
+
+
+// post data
+/* at test
+{
+    "body": "data daa body...",
+    "userHandle" : "userHadle4"
+}
+*/
+exports.postOneScream = (req, res) => {
+
+    if(req.body.body.trim() === ""){
+        return res.status(400).json({body: 'Body must not be empty'})
+    }
+
+  const newScreams = {
+    body: req.body.body,
+    //userHandle: req.body.userHandle,
+    userHandle: req.user.handle,// passed through FBAuth
+    //createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString(),
   };
 
-  exports.postOneScream = (request, response) => {
-    if(request.body.body.trim() === '') {
-      return response.status(400).json({body: 'Body must not be empty'});
-    }
-    const newScream = {
-      body: request.body.body,
-      //FBAuth
-      //userHandle: request.body.userHandle,
-      userHandle: request.user.handle,
-      //createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-      uceatedAt: new Date().toISOString()
-    };
-  
-    db
-      .collection("screams")
-      .add(newScream)
-      .then((doc) => {
-        response.json({ message: `document ${doc.id} cteated successfully` });
-      })
-      .catch((err) => {
-        response.status(500).json({ error: "something went wrong" });
-        console.error(err);
-      });
-  };
+  db
+    .collection("screams")
+    .add(newScreams)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ eror: "something went wrong" });
+      console.error(err);
+    });
+}
--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,109 +1,75 @@
-const { db } = require('../utils/admin');
-
+const {admin, db} = require('../utils/admin');
 const config = require('../utils/config');
-
 const firebase = require('firebase');
 firebase.initializeApp(config);
 
-const  { validateSignupData, validateLoginData} = require('../utils/validators');
+const { validateSignupData, validateLoginData, reduceUserDetails } = require('../utils/validators');
+const { UserRecordMetadata } = require('firebase-functions/lib/providers/auth');
 
-exports.signup = (request, response) => {
-    const newUser = {
-      email: request.body.email,
-      password: request.body.password,
-      confirmPassword: request.body.confirmPassword,
-      handle: request.body.handle,
-    };
-  
-    const { valid, errors } = validateSignupData(newUser);
-
-    if(!valid) return response.status(400).json(errors);
-
-    //validate data
-    let token, userId;
-    //db.collection('users').doc()
-    db.doc(`/users/${newUser.handle}`)
-      .get()
-      .then((doc) => {
-        if (doc.exists) { //handle이 존재하면 리턴
-          return response
-            .status(400)
-            .json({ handle: "this handle is already taken" });
-        } else { // handle 존재하지 않으면 새로운 계정 만들기
-          return firebase
-            .auth()
-            .createUserWithEmailAndPassword(newUser.email, newUser.password);
-        }
-      })
-      .then((data) => {
-        userId = data.user.uid;
-        return data.user.getIdToken();
-      })
-      .then((idToken) => {
-        token = idToken;
-        //return response.status(201).json({ token });
-        const userCredentials = {
-          handle: newUser.handle,
-          email: newUser.email,
-          createdAt: new Date().toISOString(),
-          //userId: userId
-          userId,
-        };
-        return db
-          .doc(`/users/${newUser.handle}`)
-          .set(userCredentials);
-      })
-      .then(() => {
-        return response.status(201).json({ token });
-      })
-      .catch((err) => {
-        console.error(err);
-        if (err.code === "auth/email-already-in-use") {
-          return response.status(400).json({ email: "email is already exist" });
-        } else {
-          return response.status(500).json({ error: err.code });
-        }
-      });
+//////////////////
+exports.signup = (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
   };
 
-//   exports.login = (request, response) => {
-//     const user = {
-//       email: request.body.email,
-//       password: request.body.password,
-//     };
+  const { valid, errors } = validateSignupData(newUser);
+  if(!valid) return res.status(400).json(errors);
 
-//     const { valid, errors } = validateLoginData(user);
+  const noImg = 'no-img.png';
 
-//     if (!valid) return response.status(400).json(errors);
-
-//     firebase
-//       .auth()
-//       .signInWithEmailAndPassword(user.email, user.password)
-//       .then((data) => {
-//         return data.user.getIdToken();
-//       })
-//       .then((token) => {
-//         return response.json({ token });
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//         if (err.code === "auth/wrong-password") {
-//           return response
-//             .status(403)
-//             .json({ general: "Wrong credentials, please try again" });
-//         } else {
-//           return response.status(500).json({ error: err.code });
-//         }
-//       });
-//   };
+  // TODO: validate data;
+  // db:usrers handle <=> auth: userID
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      //if registered at users then err
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else { // not registered user then create new users a
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => { 
+        userId = data.user.uid;
+        return data.user.getIdToken();
+    })
+    .then((idToken) => {// step 7
+        //return res.status(201).json({ token });
+        token = idToken;
+        const userCredentials = {
+            handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
+            userId // auth=>uid = users=>userId
+        };
+        // setting user credentials at (db => users)
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);    
+    })
+    .then(() => {
+        return res.status(201).json({token});
+    })
+    .catch((err) => {
+      console.error(err);
+      if(err.code === 'auth/email-already-in-use'){
+          res.status(400).json( {email: 'Email is already is use'})
+      } else {
+        return res.status(500).json({ error: err.code });
+        }
+    });
+}
 
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
         password: req.body.password
     };
-
-    
 
     const { valid, errors } = validateLoginData(user);
     if(!valid) return res.status(400).json(errors);
@@ -131,3 +97,110 @@ exports.login = (req, res) => {
             return res.status(500).json({ error: err.code });
       });
 }; 
+  
+ 
+
+
+
+
+
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection("likes")
+          .where("userHandle", "==", req.user.handle)
+          .get();
+      }
+    })
+    .then((data) => {
+      userData.likes = [];
+      data.forEach((doc) => {
+        userData.likes.push(doc.data());
+      });
+      
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+
+
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body);
+
+  db.doc(`/users/${req.user.handle}`).update(userDetails)
+    .then(() => {
+      return res.json({message: 'Details added successufly'});
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({error: err.code});
+    });
+};
+
+// Upload a profile image for user
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageToBeUploaded = {};
+  let imageFileName;
+  // String for image token
+  let generatedToken = uuid();
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname, file, filename, encoding, mimetype);
+    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+      return res.status(400).json({ error: "Wrong file type submitted" });
+    }
+    // my.image.png => ['my', 'image', 'png']
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    // 32756238461724837.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+            //Generate token to be appended to imageUrl
+            firebaseStorageDownloadTokens: generatedToken,
+          },
+        },
+      })
+      .then(() => {
+        // Append token to url
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media&token=${generatedToken}`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "image uploaded successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: "something went wrong" });
+      });
+  });
+  busboy.end(req.rawBody);
+};
