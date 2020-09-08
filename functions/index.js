8971f146516f37455b4333b39bd4433cc7760b91
KyeongHoSeong <dasomaru@gmail.com>
2020-09-08 14:48:45 +0900

    lesson-04--user Registeration router

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,30 @@
 const functions = require('firebase-functions');
-const admin =require('firebase-admin');
+const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyCysldo30CtHr2GqAGnBKhkh8dEfxDDlO0",
+  authDomain: "firereact-f2000.firebaseapp.com",
+  databaseURL: "https://firereact-f2000.firebaseio.com",
+  projectId: "firereact-f2000",
+  storageBucket: "firereact-f2000.appspot.com",
+  messagingSenderId: "436517320273",
+  appId: "1:436517320273:web:b808a7ad1d8bef2d186a70",
+  measurementId: "G-H63ZZWD2JM"
+};
 
-// exports.getScreams = functions.https.onRequest((request, response) => {
-//   admin.firestore().collection('screams').get()
-//     .then(data => {
-//       let screams = [];
-//       data.forEach(doc => {
-//         screams.push(doc.data());
-//       });
-//       return response.json(screams);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//     });
-// });
 
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+
+const db = admin.firestore();
 
 app.get('/screams', (request, response) => {
-  admin
-    .firestore()
+  db
     .collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
@@ -46,35 +47,6 @@ app.get('/screams', (request, response) => {
 }) 
 
 
-
-
-// exports.createScream = functions.https.onRequest((request, response) => {
-//   if(request.method !== 'POST'){
-//     return response.status(400).json({error: "Method not allowed"});
-//   }
-
-//   const newScream = {
-//     body: request.body.body,
-//     userHandle: request.body.userHandle,
-//     createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-//   };
-
-//   admin
-//     .firestore()
-//     .collection("screams")
-//     .add(newScream)
-//     .then((doc) => {
-//       response.json({ message: `document ${doc.id} cteated successfully` });
-//     })
-//     .catch((err) => {
-//       response.status(500).json({ error: "something went wrong" });
-//       console.error(err);
-//     });
-// });
-
-
-
-
 app.post('/scream', (request, response) => {
   const newScream = {
     body: request.body.body,
@@ -83,8 +55,7 @@ app.post('/scream', (request, response) => {
     uceatedAt: new Date().toISOString()
   };
 
-  admin
-    .firestore()
+  db
     .collection("screams")
     .add(newScream)
     .then((doc) => {
@@ -96,6 +67,71 @@ app.post('/scream', (request, response) => {
     });
 });
 
-// https://baseurl.com/api/getScreams
-//exports.api = functions.https.onRequest(app);
+//signup route
+
+app.post("/signup", (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle,
+  };
+
+  //validate data
+  let token, userId;
+  //db.collection('users').doc()
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) { //handle이 존재하면 리턴
+        return response
+          .status(400)
+          .json({ handle: "this handle is already taken" });
+      } else { // handle 존재하지 않으면 새로운 계정 만들기
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      //return response.status(201).json({ token });
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        //userId: userId
+        userId,
+      };
+      return db
+        .doc(`/users/${newUser.handle}`)
+        .set(userCredentials);
+    })
+    .then(() => {
+      return response.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return response.status(400).json({ email: "email is already exist" });
+      } else {
+        return response.status(500).json({ error: err.code });
+      }
+    });
+});
+
+  // firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.passowrd)
+  //   .then(data => {
+  //     return response.status(200).json({ message: `user ${data.user.uid} signed up successuflly`});
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //     return response.status(500).json({error: err.code});
+  //   })
+
+
 exports.api = functions.region('asia-northeast3').https.onRequest(app);
